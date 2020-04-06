21ca7e17c3bbd0f41c293ac4e88ccfc98f67ee5e
Mykhailo Ivanov <iv09mik@gmail.com>
2020-04-06 11:11:22 +0300

    Task is done

--- Program.cs
+++ Program.cs
@@ -3,6 +3,27 @@ using CodingCampusCSharpHomework;
 
 namespace HomeworkTemplate
 {
+    class SuperVirus : Task5.Virus {
+        public SuperVirus() : base(false)
+        {
+            DateTimeOfCreation = DateTime.Now;
+        }
+        public override string Name
+        {
+            get => name;
+            set { name = value.Replace("virus", "supervirus"); }
+        }
+        public override string VictimName { get; set; }
+        public override float KillProbability
+        {
+            get { return killProbability; }
+            set { killProbability = Math.Clamp(value * 2, 0f, 1f); }
+        }
+        public override DateTime DateTimeOfCreation { get; }
+
+        private string name;
+        private float killProbability;
+    }
     class Program
     {
         static void Main(string[] args)
